66588b0214e5b8a6aae745015881d68f8d23b000
yarynas21 <savkiv.pn@ucu.edu.ua>
2023-12-26 17:44:42 +0200

    new

--- src/main/java/apps/ucu/edu/ua/tasksecond/Database.java
+++ src/main/java/apps/ucu/edu/ua/tasksecond/Database.java
@@ -1,6 +1,6 @@
 package apps.ucu.edu.ua.tasksecond;
 
-public class Database extends БазаДаних{
+public class Database extends БазаДаних {
     public String getUserData() {
         return отриматиДаніКористувача();
     }
